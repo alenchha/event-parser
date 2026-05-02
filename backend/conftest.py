5043baeb9e9ce7e968ad1a4a62d359dc901cfd0e
alenchha <a.kurlowa@yandex.ru>
2026-05-02 20:19:25 +0300

    change conftest

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
