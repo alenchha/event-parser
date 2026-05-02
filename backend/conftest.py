8faef6f552a66f0e1d7895ee9e62a922e2e0b450
alenchha <a.kurlowa@yandex.ru>
2026-05-02 20:23:39 +0300

    fix: change ci-cd

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,8 +1,3 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
